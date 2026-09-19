ec8e8e30f4669edd517d3d561ab1f39f8df58454
lestergarcia711 <lestergarcia711@gmail.com>
2026-09-19 15:28:56 -0600

    feat:Mejorar y optimizar el codigo de Student Repository.js

--- src/repositories/StudentRepository.js
+++ src/repositories/StudentRepository.js
@@ -1,9 +1,10 @@
-import { pool } from '../config/database.';
+import { pool } from '../config/database.js';
 import { Student } from '../models/Student.js';
 
 export class StudentRepository{
 
     #mapToEntity(row){
+        if(!row)return null;
         return new Student({
             id: row.id,
             code: row.code,
@@ -19,20 +20,32 @@ export class StudentRepository{
         });
     }
     async findAll(){
-        const query = 'SELECT * FROM students';
+        try{
+            const query = 'SELECT * FROM students';
         const [rows]= await pool.query(query);
         return rows.map(row => this.#mapToEntity(row));
+        }catch(error){
+            throw new Error(`Error al obtener estudiantes: ${error.message}`);
+        }
     }
+
     async findById(id){
-        const query = 'SELECT * FROM students WHERE id = ?';
+        try {
+            const query = 'SELECT * FROM students WHERE id = ?';
         const [rows]= await pool.query(query, [id]);
         
         if (rows.length === 0) return null;
         return this.#mapToEntity(rows[0]);
+        }catch(error){
+            throw new Error(`Error al buscar estudiante por ID $i{id}: ${error.message}`);
+
+        }
     }
 
     async save(studentData){
-        const query = `INSERT INTO students
+     try{
+           const query = 
+        `INSERT INTO students
         (code, firstName, lastName, identification_type_id, identificationNumber,
         gender, birthdate, email, address, city_id)
         VALUES(?, ? , ?, ?, ?, ?, ?, ?, ?, ?)`;
@@ -49,8 +62,11 @@ export class StudentRepository{
             studentData.cityId
         ];
 
-        const [results] = await pool.execute(query,params);
+        const [result] = await pool.execute(query,params);
         return result.insertId;
+     }catch(error){
+        throw new Error(`Error al guardar el estudiante: ${error.message}`);
+     }
     }
 
 
